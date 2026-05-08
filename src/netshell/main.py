1420b0f669d5a98618ef2792656419c00d447112
Richard A. Dubniczky <richard@dubniczky.com>
2026-05-08 10:55:19 +0200

    Bump version to 1.1.1

--- src/netshell/main.py
+++ src/netshell/main.py
@@ -13,7 +13,7 @@ from prompt_toolkit.history import FileHistory
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 
 
-VERSION = "1.1.0"
+VERSION = "1.1.1"
 HISTORY_LOCATION = ".netshell_history"
 QUERY_PLACEHOLDER = "--NETSHELL_PLACEHOLDER--"
 
